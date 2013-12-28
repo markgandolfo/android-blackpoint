eef10c0bf8b600ded6a2f791c3fdbb613d52cf18
Mark Gandolfo <mark@aussiev8.com.au>
2013-12-28 22:05:50 +1000

    adding sprite inlieu of box

--- Blackpoint/src/com/gan/blackpoint/screens/Play.java
+++ Blackpoint/src/com/gan/blackpoint/screens/Play.java
@@ -6,6 +6,10 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
@@ -16,6 +20,7 @@ import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 import com.gan.blackpoint.InputController;
 
 public class Play implements Screen {
@@ -23,45 +28,62 @@ public class Play implements Screen {
 	private World world;
 	private Box2DDebugRenderer debugRenderer;
 	private OrthographicCamera camera;
-
+	private Sprite boxSprite;
+	private SpriteBatch batch;
+	private Body box;
+	private Array<Body> tmpBodies = new Array<Body>();
+	
 	private final float PIXELS_TO_METERS = 15f,			// how many pixels to a meter
 						TIME_STEP = 1 / 60f, 			// 60 fps
 						SPEED = 500f,					// speed constant
-						MIN_ZOOM = .25f;				// How far in should we be able to zoom
-	
+						MIN_ZOOM = .25f,				// How far in should we be able to zoom
+						ANGULAR_MOMENTUM = .50f;
 	
 	private final int 	VELOCITY_ITERATIONS = 8,		// copied from box2d example
 						POSITION_ITERATIONS = 3;		// copied from box2d example
 	
 	private Vector2 movement = new Vector2();
-	
-	private Body box;
+	private float spin = 0;
 	
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0,0,0,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
-		debugRenderer.render(world,  camera.combined);
+		world.step(TIME_STEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
 		box.applyForceToCenter(movement, true);
+		box.applyAngularImpulse(spin, true);
 
 		camera.position.set(box.getPosition().x,  box.getPosition().y, 0);
 		camera.update();
 		
-		world.step(TIME_STEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		world.getBodies(tmpBodies);
+		for(Body body : tmpBodies) {
+			if(body.getUserData() instanceof Sprite) {
+				Sprite sprite = (Sprite) body.getUserData();
+				sprite.setPosition(body.getPosition().x - sprite.getWidth() / 2, body.getPosition().y - sprite.getHeight() / 2);
+				sprite.setRotation(body.getAngle() * MathUtils.radiansToDegrees);
+				sprite.draw(batch);
+			}
+		}
+		batch.end();
+		
+		debugRenderer.render(world,  camera.combined);
 	}
 
 	@Override
 	public void resize(int width, int height) {
 		camera.viewportWidth = width / PIXELS_TO_METERS;
 		camera.viewportHeight = height / PIXELS_TO_METERS;
-		camera.update();
 	}
 
 	@Override
 	public void show() {
 		world = new World(new Vector2(0, -9.81f), true);
 		debugRenderer = new Box2DDebugRenderer();
+		batch = new SpriteBatch();
 
 		camera = new OrthographicCamera();
 
@@ -84,6 +106,11 @@ public class Play implements Screen {
 					case Keys.D:
 						movement.x = SPEED;
 						break;
+					case Keys.Q:
+						spin = ANGULAR_MOMENTUM;
+						break;
+					case Keys.E:
+						spin = -ANGULAR_MOMENTUM;
 				}
 				return true;	
 			}
@@ -97,6 +124,10 @@ public class Play implements Screen {
 				case Keys.A:
 				case Keys.D:	
 					movement.x = 0;
+					break;
+				case Keys.Q:
+				case Keys.E:
+					spin = 0;
 					break;
 			}
 			return true;	
@@ -130,10 +161,15 @@ public class Play implements Screen {
 		fixtureDef.friction = .25f;
 		fixtureDef.restitution = .1f;
 		fixtureDef.density = 5;		// 5kg in a square meter
-		
+				
 		box = world.createBody(bodyDef);
 		box.createFixture(fixtureDef);
-				
+
+		boxSprite = new Sprite(new Texture("img/player.png"));
+		boxSprite.setSize(2, 2);
+		boxSprite.setOrigin(boxSprite.getWidth() / 2, boxSprite.getHeight() / 2);
+		box.setUserData(boxSprite);
+
 		// Ball Shape
 		CircleShape ballShape = new CircleShape();
 		ballShape.setRadius(.5f);
@@ -188,7 +224,6 @@ public class Play implements Screen {
 	public void dispose() {
 		world.dispose();
 		debugRenderer.dispose();
-		
 	}
 
 }
